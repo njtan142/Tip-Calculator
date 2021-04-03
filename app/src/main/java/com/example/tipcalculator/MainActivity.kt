96aeef97adef77c78df34b549e19685480a12cf5
njtan142 <njtan142@gmail.com>
2021-04-03 22:41:10 +0800

    New design and improved functionality

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.tipcalculator
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.lang.Exception
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -15,46 +21,71 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCalculate.setOnClickListener(){
+        binding.btnCalculate.setOnClickListener() {
             calculateTip()
         }
+
         binding.etCustomTip.setOnClickListener() {
-            var rgTips = binding.rgTips
-            rgTips.check(R.id.rbCustomTip)
+            binding.rgTips.check(R.id.rbCustomTip)
+        }
+
+        binding.etCustomTip.setOnFocusChangeListener { _, _ ->
+            binding.rgTips.check(R.id.rbCustomTip)
+        }
+
+        binding.etCostOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+        binding.etCustomTip.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+
+    }
+
+    private fun calculateTip() {
+        try {
+            val tipPercent = when (binding.rgTips.checkedRadioButtonId) {
+                R.id.rb15pTip -> 15
+                R.id.rb18pTip -> 18
+                R.id.rb20pTip -> 20
+                else -> binding.etCustomTip.text.toString().toFloat()
+            }
+            val tip = Tip(
+                binding.etCostOfService.text.toString().toInt(),
+                tipPercent = tipPercent.toFloat(),
+            )
+            val amount = tip.getTip(binding.switchRoundTipToggle.isChecked)
+            val formattedAmount = NumberFormat.getCurrencyInstance().format(amount)
+            binding.tvTipAmount.text = getString(R.string.tip_amount, formattedAmount)
+        } catch (e: Exception) {
+            // do nothing
         }
     }
 
-    private fun calculateTip()
-    {
-        val tipPercent = when(binding.rgTips.checkedRadioButtonId){
-            R.id.rb15pTip -> 15
-            R.id.rb18pTip -> 18
-            R.id.rb20pTip -> 20
-            else -> binding.etCustomTip.text.toString().toFloat()
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
         }
-        val tip = Tip(
-            binding.etCostOfService.text.toString().toInt(),
-            tipPercent = tipPercent.toFloat(),
-        )
-        val amount = tip.getTip(binding.switchRoundTipToggle.isChecked)
-        val formattedAmount = NumberFormat.getCurrencyInstance().format(amount)
-        binding.tvTipAmount.text = getString(R.string.tip_amount, formattedAmount)
+        return false
     }
+
 
 }
+
 
 class Tip(
-    var cost : Int,
-    var tipPercent : Float,
-){
+    var cost: Int,
+    var tipPercent: Float,
+) {
 
-    fun getTip(rounded: Boolean) : Any
-    {
-        return if(rounded) {
-            kotlin.math.ceil(cost * (tipPercent/100))
+    fun getTip(rounded: Boolean): Any {
+        return if (rounded) {
+            kotlin.math.ceil(cost * (tipPercent / 100))
         } else {
-            cost * (tipPercent/100)
+            cost * (tipPercent / 100)
         }
     }
-
 }
+
